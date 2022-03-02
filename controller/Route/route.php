59977a5199fc97d2a589d1676b5e731dec2f435c
Alexandre Burlot/usr/local/bin/python3 <burlotalexandre@yahoo.fr>
2022-03-02 19:27:24 +0100

    commentaires

--- controller/Route/route.php
+++ controller/Route/route.php
@@ -3,10 +3,10 @@
 namespace Controller\Route;
 
 /**
- * classe Route qui contient un objet route avec pour parametres:
- * @param path  chemin vers le fichier a ouvrir
- * @param action fonction a executer 
- * @param matches routes qui matches avec le path
+ * classe Route qui contient un objet de type route avec pour parametres:
+ * @param path      chemin vers le fichier a ouvrir
+ * @param action    fonction a executer 
+ * @param matches   routes qui matches avec le path
  */
 class Route
 {
@@ -15,35 +15,35 @@ class Route
     public $matches;
 
     /**
-     * Méthode construsant une route et la méthode et la méthode à réaliser
-     * @param path donne le chemin d'accès
-     * @param action donne le namespace avec la méthode à appeler
+     * Méthode construisant une route avec la méthode à réaliser
+     * @param path      donne le chemin d'accès
+     * @param action    donne le namespace avec la méthode à appeler
      */
     public function __construct($path, $action)
     {   
-        # enlève le caractère '/' au début et à la fon de la chaine de caractère
+        # enlève le caractère '/' au début et à la fin de la chaine de caractère
         $this->path = trim($path, '/');
         # stocke l'action voulue en paramètre
         $this->action = $action; 
-            
     }
 
     /**
      * Methode qui vérifie si l'url passer correspond au chemin existant
-     * @param url variable de type string qui renseigne l'url
+     * @param url string, renseigne l'url
+     * @return valren boolean, montre si l'url est la même chose que le path
      */
     public function matches(string $url)
     {   
+        # Initialisation de la valeur de retour
         $valren = false;
+        # remplace le deuxième argument par le caractère ci-dessous
         $path = preg_replace('#:([\w]+)#', '([^/]+)', $this->path);
         
         $pathToMatch = "#^$path$#";
         
         
         if (preg_match($pathToMatch, $url, $matches))
-        {
-
-            
+        {            
             $this->matches = $matches;
             $valren =  true;
         }
